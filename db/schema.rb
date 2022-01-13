888214d05b25020fd1f618e2d613341b3cc69fac
1chi1en <naokkmt1234@gmail.com>
2022-01-13 21:52:27 +0900

    deviseが機能しなかったので小修正

--- db/schema.rb
+++ db/schema.rb
@@ -55,7 +55,8 @@ ActiveRecord::Schema.define(version: 2021_12_23_022540) do
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
-    t.string "name"
+    t.string "name", null: false
+    t.string "email"
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
